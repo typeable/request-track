03d2841a9d25f160fbe4f2e2e33a3f8d43ee93c8
Nick <nick@typeable.io>
2023-07-20 18:14:38 +0000

    Fix build.

--- request-track-servant/src/Web/Tracking/Servant.hs
+++ request-track-servant/src/Web/Tracking/Servant.hs
@@ -1,5 +1,4 @@
 module Web.Tracking.Servant (module Export) where
 
 import Web.Tracking as Export (ReqtrackInfo(..))
-import Web.Tracking.Servant.Handler as Export
 import Web.Tracking.Servant.Types as Export
