ec7a70f10975923410a35eb413b38aed8e9f250b
Nick <nick@typeable.io>
2023-08-15 10:51:00 +0000

    Fix module export.

--- request-track-servant/src/Web/Tracking/Servant.hs
+++ request-track-servant/src/Web/Tracking/Servant.hs
@@ -4,5 +4,5 @@ module Web.Tracking.Servant (module Export) where
 import Web.Tracking as Export (ReqtrackInfo(..))
 import Web.Tracking.Servant.Types as Export
 #ifdef OPENAPI
-import Web.Tracking.Servant.OpenAPI ()
+import Web.Tracking.Servant.OpenAPI as Export ()
 #endif
